6007e0a4f620a279064e83fcf25594706e49dddf
natarajan1993 <natrajm93@gmail.com>
2018-11-20 14:33:55 -0600

    Completed Part 9

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from .models import Profile
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -8,3 +9,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm): # Form to update the user profile
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm): # Form to update the profile pic
+    class Meta:
+        model = Profile
+        fields = ['image']
--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE) # Every user in the Profile Model is associated with a User from Django so we define that relationship with a OneToOneField
@@ -7,4 +8,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    def save(self): # Overwrite the default save method so we can scale down the image before uploading it
+        super().save()
 
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            img.thumbnail((300,300)) # Use pillow to resize the thumbnail image to 300x300
+            img.save(self.image.path)
